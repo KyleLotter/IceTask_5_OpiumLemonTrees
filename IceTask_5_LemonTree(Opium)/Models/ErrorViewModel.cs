5c673785fa99ce9c46d26036797795f04e4ddc72
KyleLotter <ST10336577@vcconnect.edu.za>
2025-10-08 13:54:40 +0200

    Add project files.

---
+++ IceTask_5_LemonTree(Opium)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IceTask_5_LemonTree_Opium_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
